0f6e16ef44e5dc3dfbf2056779041fca3725e395
Deftool66 <deftool@gmx.net>
2022-08-10 13:59:53 +0200

    added Landing page and Login panel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import React from 'react';
+//import './App.css';
+import {useSelector} from 'react-redux';
+import Login from './Views/Login/login';
+/* import authSlice from './Controllers/Redux/authSlice'; */
+
 
 function App() {
+  const {auth} = useSelector(state => state);
+  console.log(auth);
+
   return (
-<>
-</>);
+    <>
+      {auth.LoggedIn? <h1>Hello</h1> : <Login /> }
+    </>
+);
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
+
+//Reducers
+import authReducer from './Controllers/Redux/authSlice';
+import bugReducer from './Controllers/Redux/bugSlice';
+import userReducer from './Controllers/Redux/userSlice';
+
+//Reduce configure
+const reducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer
+});
+const store = configureStore({reducer});
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+      <App />
+      </Provider>
 );
